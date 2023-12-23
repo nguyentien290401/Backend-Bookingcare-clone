88a7617b6360936f143497fe97c5883e45166e5e
Tiến Nguyễn <nguyentien290401@gmail.com>
2023-12-23 15:40:09 +0700

    Create Models, Migrations and Seeders for project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-The backend of Bookingcare clone
+The backend of Bookingcare clone: https://bookingcare.vn/
+
+Some conditions to run this project:
+
+~   Using nodejs exact version 14.17.0 or lower
+~   Using XAMPP to run the database (press both Apacha and MySQL when you run the server)
+~   Installing exact library version in package.json (npm i)
+~   Running the project (npm start)
+
+
+
--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -3,9 +3,13 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
       email: 'example@example.com',
+      firstName: 'Admin',
+      lastName: 'Lumine',
+      address: 'Ho Chi Minh',
+      gender: 2,
+      typeRole: 'ROLE',
+      keyRole: 'R1',
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
